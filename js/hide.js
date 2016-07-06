0899d773f178af41c31a47d60b13cc00d0711626
jcelemin <martinezjua00@gmail.com>
2016-07-06 14:44:21 -0500

    menu ajustes en los estilos y js

--- js/hide.js
+++ js/hide.js
@@ -1,8 +1,21 @@
  var $burguerButton = document.getElementById('burguer-button');
-      var $menu = document.getElementById('menu');
+ var $menu = document.getElementById('menu');
+ var $estado =false;
 
       $burguerButton.addEventListener('click', toggleMenu);//nombre de la funcion 
 
       function toggleMenu(){
         $menu.classList.toggle('active')//nombre del a clase .css
+        if (!$estado) {
+        	$burguerButton.classList.remove('icon-indent-increase')
+        	$burguerButton.classList.add('icon-indent-decrease')
+        	$estado =true;
+
+        }
+        else{
+        	$burguerButton.classList.remove('icon-indent-decrease')
+        	$burguerButton.classList.add('icon-indent-increase')
+        	$estado =false;
+        }
+        
       };
