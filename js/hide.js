54655b7458bc21b071a01c168cc3dece96aca536
jcelemin <martinezjua00@gmail.com>
2016-07-06 01:58:35 -0500

    menu con estilos y js

--- js/hide.js
+++ js/hide.js
@@ -1,8 +1,8 @@
  var $burguerButton = document.getElementById('burguer-button');
- var $menu = document.getElementById('menu');
+      var $menu = document.getElementById('menu');
 
-      $burguerButton.addEventListener('click', toggleMenu);
+      $burguerButton.addEventListener('click', toggleMenu);//nombre de la funcion 
 
       function toggleMenu(){
-        $menu.classList.toggle('active')
+        $menu.classList.toggle('active')//nombre del a clase .css
       };
